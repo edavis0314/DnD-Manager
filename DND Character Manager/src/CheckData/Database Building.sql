ce41714f0e63ccb9dd632d9fb550604d064e5de9
edavis0314 <edavis0314@hotmail.com>
2019-04-10 21:30:13 -0400

    Add files via upload

--- DND Character Manager/src/CheckData/Database Building.sql
+++ DND Character Manager/src/CheckData/Database Building.sql
@@ -12,7 +12,6 @@ Create Table EffectTable(EffectID int not null, Effect text, primary key(EffectI
 Create Table MagicItem(MagicItemID int not null, Slots int not null, primary key(MagicItemID));
 Create Table EffectGroup(MagicItemID int not null, EffectID int not null, foreign key(MagicItemID) references MagicItem(MagicItemID), foreign key(EffectID) references EffectTable(EffectID));
 Create Table SkillProf(SkillProfID int NOT NULL, Name text NOT NULL, Type text NOT NULL, primary key(SkillProfID));
-Create Table LangAndProf(LangAndProfID int NOT NULL, Name text NOT NULL, Type text NOT NULL, NOTES text NOT NULL, primary key(LangAndProfID));
 Create Table Ally(AllyID int NOT NULL, CharacterID int NOT NULL, NOTES text NOT NULL, primary key(AllyID), foreign key(CharacterID) references Characters(CharacterID));
 Create Table Equipment(EquipmentID int not null, Name text not null, Cost varchar (20) not null, Weight int not null, MagicItemID int not null, NOTES text not null, primary key(EquipmentID), foreign key(MagicItemID) references MagicItem(MagicItemID));
 Create Table Mount(MountID int not null, Name text not null, Cost varchar (20) not null, Speed varchar (20) not null, MagicItemID int not null, CarryCapacity varchar (20) not null, NOTES text not null, primary key(MountID), foreign key(MagicItemID) references MagicItem(MagicItemID));
@@ -22,7 +21,6 @@ Create Table Weapon(WeaponID int not null, Name text not null, Cost varchar (20)
 Create Table Armor(ArmorID int not null, Name text not null, Cost varchar (20) not null, Stats text not null, MagicItemID int not null, StrRequirement int not null, Stealth bit not null, Type varchar (50) not null, Weight int not null, NOTES text not null, primary key(ArmorID), foreign key(MagicItemID) references MagicItem(MagicItemID));
 Create Table CharacterRecord(CharacterID int NOT NULL, CharacterName text NOT NULL, CharacterLevel int NOT NULL, CharacterExperience int NOT NULL, Class varchar (50) NOT NULL, Spec varchar (50), Background text NOT NULL, Alignment varchar(20) NOT NULL, Strength int NOT NULL, Dexterity int NOT NULL, Constitution int NOT NULL, Inteligence int NOT NULL, Wisdom int NOT NULL, Charisma int NOT NULL, Speed int NOT NULL, MaxHP int NOT NULL, CurrentHP int NOT NULL, TempHP int NOT NULL, ArmorClass int NOT NULL, DeathSaveSuccess int NOT NULL, DeathSaveFail int NOT NULL, SpellSave int NOT NULL, SpellAttack int NOT NULL, SpellCastingAbility varchar (500) NOT NULL, Age int NOT NULL, Height int NOT NULL, Weight int NOT NULL, Inspiration int NOT NULL, PersonalityTraits text NOT NULL, Ideals text NOT NULL, Bonds text NOT NULL, Flaws text NOT NULL, Race varchar (200) NOT NULL, CustomBackstory text NOT NULL, EyeColor varchar (100) NOT NULL, HairColor varchar (100) NOT NULL, SkinColor varchar (100) NOT NULL, Acrobatics int NOT NULL, AnimalHandling int NOT NULL, Arcana int NOT NULL, Athletics int NOT NULL, Deception int NOT NULL, History int NOT NULL, Insight int NOT NULL, Intimidation int NOT NULL, Investigation int NOT NULL, Medicine int NOT NULL, Nature int NOT NULL, Perception int NOT NULL, Performance int NOT NULL, Persuasion int NOT NULL, Religion int NOT NULL, SlightofHand int NOT NULL, Stealth int NOT NULL, Survival int NOT NULL, foreign key(CharacterID) references Characters(CharacterID));
 Create Table SkillProfRecord(CharacterID int NOT NULL, SkillProfID int NOT NULL, foreign key(CharacterID) references Characters(CharacterID), foreign key(SkillProfID) references SkillProf(SkillProfID));
-Create Table LangAndProfRecord(CharacterID int NOT NULL, LangAndProfID int NOT NULL, foreign key(CharacterID) references Characters(CharacterID), foreign key(LangAndProfID) references LangAndProf(LangAndProfID));
 Create Table EquipmentRecord(CharacterID int NOT NULL, Amount int not null, EquipmentID int NOT NULL, foreign key(CharacterID) references Characters(CharacterID), foreign key(EquipmentID) references Equipment(EquipmentID));
 Create Table MountRecord(CharacterID int NOT NULL, Amount int not null, MountID int NOT NULL, foreign key(CharacterID) references Characters(CharacterID), foreign key(MountID) references Mount(MountID));
 Create Table ArmorRecord(CharacterID int NOT NULL, Amount int not null, ArmorID int NOT NULL, foreign key(CharacterID) references Characters(CharacterID), foreign key(ArmorID) references Armor(ArmorID)); 
@@ -35,7 +33,6 @@ Create Table MagicRecord(CharacterID int NOT NULL, MagicID int NOT NULL, foreign
 /* SECTION 2: Monster Data ******************   */
 /*Create Table MonsterRecord();
 Create Table SkillProfRecord_Monster();
-Create Table LangAndProfRecord_Monster();
 Create Table EquipmentRecord_Monster();
 Create Table MountRecord_Monster();
 Create Table ArmorRecord_Monster(); 
@@ -59,7 +56,6 @@ Select * From EffectTable;
 Select * From EffectGroup;
 Select * From MagicItem;
 Select * From SkillProf;
-Select * From LangAndProf;
 Select * From Ally;
 Select * From Equipment;
 Select * From Mount;
@@ -69,7 +65,6 @@ Select * From Weapon;
 Select * From Armor;
 Select * From CharacterRecord;
 Select * From SkillProfRecord;
-Select * From LangAndProfRecord;
 Select * From EquipmentRecord;
 Select * From MountRecord;
 Select * From ArmorRecord;
